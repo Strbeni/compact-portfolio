183b3a5e7f1c54d36628f72b4f6f88040dc246a8
strbeni <amityadv747@gmail.com>
2025-11-11 10:57:02 +0530

    fixed bug

---
+++ src/fireBaseConfig.js
@@ -0,0 +1,22 @@
+// fireBaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyDqFQp1i9HceSYUjOpnDSPmrp-IafwP5Fo",
+  authDomain: "bento-3dc2e.firebaseapp.com",
+  projectId: "bento-3dc2e",
+  storageBucket: "bento-3dc2e.firebasestorage.app",
+  messagingSenderId: "133190296847",
+  appId: "1:133190296847:web:52902bb2042bdaf4bb8a7a",
+  measurementId: "G-JEV59BSN1R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+console.log(db)
+
+export { db };
